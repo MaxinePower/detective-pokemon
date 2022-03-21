d387e3d52aa7a91bcd68f7cd7a6b5efc67dc7c04
marina <mzubtsova@mail.ru>
2022-03-21 18:14:19 -0400

    Initial Pokemon commit

--- src/PokemonSelections.js
+++ src/PokemonSelections.js
@@ -1,14 +1,28 @@
 // PokemonSelections
 // import PokemonCard from "./PokemonCard";
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
-import PokemonCard from "./PokemonCard";
+// import PokemonCard from "./PokemonCard";
+
 
 function PokemonSelections() {
+    const [pokemon, setPokemon] = useState({});
+    const id = 12;
+    useEffect(() => {
+        axios({
+            url: `https://pokeapi.co/api/v2/pokemon/${id}`
+        })
+        .then( response => {
+            console.log(response.data);
+        })
+    })
     return(
         <section>
             <ul>
                 {/* pokemon array */}
                 {/* <PokemonCard /> */}
+
             </ul>
         </section>
     );
