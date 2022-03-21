055579c2734befaa5475e0e97128ef5e5b8b4d42
Maxine <maxine@power.sh>
2022-03-21 17:59:05 -0400

    added userInfo form, made api call in CrimeCases, and did a map to display CrimeFiles

--- src/CrimeFile.js
+++ src/CrimeFile.js
@@ -1,9 +1,10 @@
 // CrimeFile
 
-function CrimeFile() {
+function CrimeFile(props) {
+    console.log(props);
     return (
         <li>
-
+            <p>{props.typeOfCrime}</p>
         </li>
     )
 };
