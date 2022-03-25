6dec91b7478f712c012586682a26264bef98f229
KimberleySchubert <kimberley.schubert@gmail.com>
2022-03-25 10:40:51 -0400

    Sorry! I put the invenstigation.js back :)

---
+++ InvestigationAlgorithm.js
@@ -0,0 +1,11 @@
+// InvestigationAlgorithm
+
+function InvestigationAlgorithm() {
+    return( 
+        <div>
+            {/* <p>${investigation}</p> */}
+        </div>
+    );
+};
+
+export default InvestigationAlgorithm;
