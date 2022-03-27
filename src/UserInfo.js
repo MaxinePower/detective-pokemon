366d6dfa69a5362bbb922f6138a7310577d96780
Maxine <maxine@power.sh>
2022-03-27 13:06:46 -0400

    added 500fw to index, added a classNames and div containers for layout styling in UserInfo, styled userInfo form for page landing

--- src/UserInfo.js
+++ src/UserInfo.js
@@ -46,51 +46,53 @@ function UserInfo(props) {
                 <form action="" onSubmit={handleUserInfoSubmit}>
                 {/* user inputs name into input field */}
                 {/* save this input in a variable and also bind it to the input (controlled inputs) */}
-                    <label htmlFor="userName">Officer Name:</label>
-                    <input type="text" name="userName" id="userName" placeholder="Joshua Doe"
-                    onChange={handleNameChange}
-                    value={changingUserName}
-                    />
+                    <div className="userInputContainers">
+                        <label htmlFor="userName">Officer Name:</label>
+                        <input type="text" name="userName" id="userName" placeholder="Joshua Doe"
+                        onChange={handleNameChange}
+                        value={changingUserName}
+                        />
 
-                    {/* Form error handling */}
-                    {
-                        nameErrorStatus
-                        ? <span className="errorFlag">Please enter your name.</span>
-                        : <span className="errorFlag hidden">Please enter your name.</span>
-                    }
+                        {/* Form error handling */}
+                        {
+                            nameErrorStatus
+                            ? <span className="errorFlag">Please enter your name.</span>
+                            : <span className="errorFlag hidden">Please enter your name.</span>
+                        }
+                    </div>
+                    <div className="userInputContainers">
+                        {/* user selects a location from a drop down menu */}
+                        <label htmlFor="locationDropDown">Select Location:</label>
+                        <select
+                        name="locationDropDown"
+                        id="locationDropDown"
+                        // trying an inline callback function for event listener
+                        onChange={handleLocationChange}
+                        defaultValue='placeholder'
+                        >
+                            <option value="placeholder" disabled>Pick one:</option>
+                            <option value="960157">Abbey Road, London</option>
+                            <option value="964955">Camden Town, London</option>
+                            <option value="956143">Covent Garden, London</option>
+                            <option value="928868">Croydon, London</option>
+                            <option value="956559">Piccadilly Circus, London</option>
+                            <option value="538862">Bath, Somerset</option>
+                            <option value="560578">Cambridge, Cambridgeshire</option>
+                            <option value="1080958">Cardiff, Wales</option>
+                            <option value="1489701">Edinburgh, Scotland</option>
+                            <option value="1296059">Leeds, West Yorkshire</option>
 
-                    {/* user selects a location from a drop down menu */}
-                    {/* drop down menu contains 5(or more) pre-selected locations from the API endpoint - **(to be determined)** */}
-                    <label htmlFor="locationDropDown">Select Location:</label>
-                    <select
-                    name="locationDropDown"
-                    id="locationDropDown"
-                    // trying an inline callback function for event listener
-                    onChange={handleLocationChange}
-                    defaultValue='placeholder'
-                    >
-                        <option value="placeholder" disabled>Pick one:</option>
-                        <option value="960157">Abbey Road, London</option>
-                        <option value="964955">Camden Town, London</option>
-                        <option value="956143">Covent Garden, London</option>
-                        <option value="928868">Croydon, London</option>
-                        <option value="956559">Piccadilly Circus, London</option>
-                        <option value="538862">Bath, Somerset</option>
-                        <option value="560578">Cambridge, Cambridgeshire</option>
-                        <option value="1080958">Cardiff, Wales</option>
-                        <option value="1489701">Edinburgh, Scotland</option>
-                        <option value="1296059">Leeds, West Yorkshire</option>
 
+                        </select>
+                        {/* Form error handling */}
+                        {
+                            locationErrorStatus
+                            ? <span className="errorFlag">Please choose a location.</span>
+                            : <span className="errorFlag hidden">Please choose a location.</span>
+                        }
+                    </div>
 
-                    </select>
-                    {/* Form error handling */}
-                    {
-                        locationErrorStatus
-                        ? <span className="errorFlag">Please choose a location.</span>
-                        : <span className="errorFlag hidden">Please choose a location.</span>
-                    }
-
-                    <button type="submit">View Your Cases</button>
+                    <button type="submit" className="viewCasesButton">View Your Cases</button>
                 </form>
             </section>
             {
